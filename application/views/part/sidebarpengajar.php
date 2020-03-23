184a96a5286da5d7f2932cadf2ecde622b0d63c9
Dicky14 <alvinodicky548@gmail.com>
2020-03-23 22:11:31 +0700

    first commit

---
+++ application/views/part/sidebarpengajar.php
@@ -0,0 +1,58 @@
+ <!-- MENU SIDEBAR-->
+ <aside class="menu-sidebar2">
+            <div class="logo">
+                <a href="#">
+                    <img src="images/icon/logo-white.png" alt="Cool Admin" />
+                </a>
+            </div>
+            <div class="menu-sidebar2__content js-scrollbar1">
+                <div class="account2">
+                <div class="image img-cir img-120">
+                        <img src="images/icon/avatar-big-01.jpg" alt="<?php echo $nama?>" />
+                    </div>
+                    <h4 class="name"><?php echo $nama?></h4>
+                    <a href="<?= base_url('user/logout')?>">Sign out</a>
+                </div>
+                <nav class="navbar-sidebar2">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>Beranda</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>Pesan</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>Jadwal Matapelajaran</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>Tugas</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>Materi</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>Ujian</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>filter Pengajar</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>filter Siswa</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-shopping-basket"></i>Absen</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <!-- END MENU SIDEBAR-->
